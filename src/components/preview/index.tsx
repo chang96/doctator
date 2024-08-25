d3cc8e07ef819e135ef9a02b59aa97776e599af2
rahman Orewole <rorewole@gmail.com>
2024-08-25 17:37:45 +0100

    preview page setup

---
+++ src/components/preview/index.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+import { RootState } from '../../store/store';
+import styles from './styles.module.css';
+import { useSelector } from 'react-redux';
+
+export default function Preview(): JSX.Element{
+    const json = useSelector((state: RootState) => state.config.generatedJson);
+    const [state, setState] = useState({value: json})
+    useEffect(()=> {
+        setState({value: json});
+    }, [json])
+    return <div className={styles.f} >
+        <textarea onChange={()=>''} value={JSON.stringify(state.value, null, "   ")} style={{width: "100%", height: "95%", border:"0.001px solid black", borderRadius: "10px"}}></textarea>
+    </div>
+} 
