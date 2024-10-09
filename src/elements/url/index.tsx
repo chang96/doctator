1c56103e9003a0b468e6b3eea1403cd75567eef0
rahman Orewole <rorewole@gmail.com>
2024-10-09 12:42:22 +0100

    select url

--- src/elements/url/index.tsx
+++ src/elements/url/index.tsx
@@ -1,15 +1,24 @@
 import "../elements.css"
 import { useState } from "react";
 import { getProject } from "../../utils/localstorageFuncs";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../store/store";
+import { setBaseUrl } from "../../slices/request";
+
 type Servers = { url: string };
 
 function Url (){
     const projectName = "defaultProject";
     const projectConfiguration = getProject(projectName)
-    const serverArr = projectConfiguration.config.servers as Servers[]
+    const serverArr = [{url: "select base url"}, ...projectConfiguration.config.servers] as Servers[]
 
     const [state] = useState<{servers: Servers[]}>({servers: serverArr})
-    return <select>
+    const dispatch: AppDispatch = useDispatch()
+    const handleUrlChange: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
+        const {value} = e.target
+        dispatch(setBaseUrl({baseUrl: value}))
+    }
+    return <select onChange={handleUrlChange}>
         {state.servers.map(({url}, index) => {
             return <option key={index}>
                 {url}
